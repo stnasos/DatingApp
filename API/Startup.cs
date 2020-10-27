55cd90b68770214f90f054221c092e1ebb281cdb
st.nasos <st.nasos@gmail.com>
2020-10-27 21:57:51 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,7 +27,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -52,6 +52,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
